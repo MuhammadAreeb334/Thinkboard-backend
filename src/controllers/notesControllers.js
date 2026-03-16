c1d969c7fe740f0a49a93f7bcce7ed7b967c27ab
Areeb <muhammadareeb334@gmail.com>
2026-03-16 16:14:02 +0500

    Completed Backend API's

--- src/controllers/notesControllers.js
+++ src/controllers/notesControllers.js
@@ -1,15 +1,66 @@
-export const getAllNotes = (req, res) => {
-  res.status(200).send("You fetched 5 notes!");
+import Note from "../models/Note.js";
+
+export const getAllNotes = async (_, res) => {
+  try {
+    const notes = await Note.find().sort({ createdAt: -1 });
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error Fetching all notes", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-export const createNote = (req, res) => {
-  res.status(201).json({ message: "Note created sucessfully!" });
+export const getNoteById = async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).json({ message: "No Note Found" });
+    res.status(200).json(note);
+  } catch (error) {
+    console.error("Error Fetching the note", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-export const updateNote = (req, res) => {
-  res.status(200).json({ message: "Note Updated successfully" });
+export const createNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const newNote = new Note({ title, content });
+    await newNote.save();
+    return res.status(201).json({ message: "New Note created successfully" });
+  } catch (error) {
+    console.error("Error Creating the notes", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-export const deleteNote = (req, res) => {
-  res.status(200).json({ message: "Note deleted successfully" });
+export const updateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        content,
+      },
+      { returnDocument: "after" },
+    );
+    if (!updatedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    console.error("Error Updating the notes", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const deleteNote = async (req, res) => {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json({ message: "Note Deleted Successfully" });
+  } catch (error) {
+    console.error("Error Deleting the notes", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
--- src/server.js
+++ src/server.js
@@ -2,17 +2,20 @@ import express from "express";
 import notesRouter from "../src/routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
-console.log(process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
+app.use(express.json());
+app.use(rateLimiter);
 app.use("/api/notes", notesRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
 });
