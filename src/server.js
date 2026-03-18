7ee1fc860ddab3d5714cac971b24089f6efa64ec
Areeb <muhammadareeb334@gmail.com>
2026-03-18 12:49:08 +0500

    Updated Route URL

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
-app.use("/api/notes", notesRouter);
+app.use("/", notesRouter);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
