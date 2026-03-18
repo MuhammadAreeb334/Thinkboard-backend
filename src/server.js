07be2a08ef35365d292887b0028b3f8bcab9257d
Areeb <muhammadareeb334@gmail.com>
2026-03-18 13:19:11 +0500

    Addded Cors Origin

--- src/server.js
+++ src/server.js
@@ -10,7 +10,28 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://thinkboard-frontend-olive.vercel.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      return callback(new Error("No origin not allowed"));
+    }
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Blocked by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/", notesRouter);
